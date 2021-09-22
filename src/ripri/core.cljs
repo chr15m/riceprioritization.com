7b9a6965bdf658f4b3a9eea37e5a361c12b07407
Chris McCormick <chris@mccormick.cx>
2021-09-22 19:49:25 +0800

    Design and doc.

--- src/ripri/core.cljs
+++ src/ripri/core.cljs
@@ -30,11 +30,10 @@
                            (make-change-handler state [:rows r k])
                            props)]])
 
-(defn component-main [state]
-  [:div
-   [:h1 "RICE Prioritization Framework"]
-   [:p (:blurb @state)]
-   [:p [:input (merge {:placeholder "Project name"} (make-change-handler state [:project-name]))]]
+(defn component-app [state]
+  [:section#interface
+   [:h2 "Priority score calculator"]
+   [:p [:input (merge {:placeholder "Things being ranked"} (make-change-handler state [:project-name]))]]
    [:p "Effort in "
     [:select (make-change-handler state [:units :effort])
      [:option "---"]
@@ -52,10 +51,10 @@
     [:thead
      [:tr
       [:th "Name"]
-      [:th "Reach"]
-      [:th "Impact"]
-      [:th "Confidence"]
-      [:th "Effort"]
+      [:th [:a {:href "#reach"} "Reach"]]
+      [:th [:a {:href "#impact"} "Impact"]]
+      [:th [:a {:href "#confidence"} "Confidence"]]
+      [:th [:a {:href "#effort"} "Effort"]]
       [:th "Score"]]]
     [:tbody
      (for [r (range (inc (count (@state :rows))))]
@@ -80,7 +79,44 @@
             [:option {:value "50"} "low"]]]
           [component-column state r :effort {:type :number :placeholder "1000"}]
           [:td.score (if (js/isNaN score) "" score)]]))]]
-   [:button {:on-click #(swap! state update-in [:rows] sort-rows)} "sort"]
+   [:div#sort
+    [:button {:on-click #(swap! state update-in [:rows] sort-rows)} "sort"]]])
+
+(defn component-documentation [state]
+  [:section#documentation
+   [:h2 "Documentation"]
+   [:h3 [:a {:name "reach"}] "Reach"]
+   [:p "How many " (or (-> @state :units :reach) "people") " this will affect per " (or (-> @state :units :reach-time) " time period") "?"]
+   [:p "For example this might be \"100 customers per quarter\". Or \"1000\" hits per month."]
+   [:h3 [:a {:name "impact"}] "Impact"]
+   [:p "How much will this impact the reach?"]
+   [:p "For example, \"increase adoption when a person uses this\". Choose from huge (3), high (2), mid (1), low (0.5), or tiny (0.25) impact."]
+   [:h3 [:a {:name "confidence"}] "Confidence"]
+   [:p "How confident are you about your estimates for Reach, Impact, and Effort?"]
+   [:p "The values are high (100%), mid (80%), and low (50%).
+       Anything below this is too uncertain. Gather more data and come back."]
+   [:h3 [:a {:name "effort"}] "Effort"]
+   [:p "How much effort is this going to require in " (or (-> @state :units :effort) "people time-units") "?"]
+   [:p "For example two people taking four weeks would be 8 person weeks of effort."]
+   [:p "You can enter any value here but try to stick to whole numbers, or use a smaller person-time-unit."]
+   [:h3 "Calculation"]
+   [:p "Finally, here's a reminder of the RICE score caluculation: " [:code "(R x I x C) / E"]]
+   [:p "The calculator above will use your numbers to sort your options into priority order."]])
+
+(defn component-main [state]
+  [:div
+   [:header
+    [:h1 "RICE Prioritization Framework"]
+    [:p (:blurb @state)]
+    [:p "The basic formula this prioritization method uses is to multiply Reach, Impact, and Confidence, and then divide by Effort."
+     [:pre "(R x I x C) / E"]
+     "This gives a single score that you can use to rank projects, tasks, or other options.
+     Use the calculator below to find your score and priority order for yours."]
+    [:p "You can read more about how RICE works "
+     [:a {:href "https://www.intercom.com/blog/rice-simple-prioritization-for-product-managers/"}
+      "in this blog post"] ". Or see the documentation below."]]
+   [component-app state]
+   [component-documentation state]
    [:footer
     [:p [:a {:href "mailto:chris@mccormick.cx?subject=riceprioritization.com feedback"} "Feature request?"]]   
     [:p "Made with 🤖 by " [:a {:href "https://twitter.com/mccrmx" :taret "_BLANK"} "@mccrmx"]]]])
